fba550c086bf14bdf927a80844aa2cf1834c9f8e
Khushal Yadav <khushalyadav535@gmail.com>
2025-06-17 16:40:18 +0530

    update backend

--- smsbackend/config/db.js
+++ smsbackend/config/db.js
@@ -21,11 +21,7 @@ console.log('Database Configuration:', {
 });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  uri: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
